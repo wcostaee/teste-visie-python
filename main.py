4955ca9159caa00c7aae14be7a6a105caf988f63
Wellinton Costa <wcostaee@gmail.com>
2021-02-28 00:27:53 -0300

    A tabela busca os valores atuais do banco de dados A tabela está 'zebrada'

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from flask import Flask, url_for, render_template
+from database import Database
+
 app = Flask(__name__)
+db = Database()
 
 @app.route("/")
 def main():
-    return render_template("home.html")
+    registros = db.search_all()
+    return render_template("home.html", registros=registros)
+
+# @app.route("/style.css")
+# def static_css():
+#     return url_for("static", filename="style.css")
