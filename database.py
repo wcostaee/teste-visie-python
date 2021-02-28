7bf92838d5357f8e23af8e4b6745ac3ead18ec83
Wellinton Costa <wcostaee@gmail.com>
2021-02-27 23:27:34 -0300

    Adição da função de inserção de uma nova coluna na tabela

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import datetime
 
 class Database:
     def __init__(self):
@@ -11,8 +12,26 @@ class Database:
         self.__cursor = self.__db.cursor()
 
     def search_all(self):
-        self.__cursor.execute("SELECT nome, data_admissao FROM " + self.__table_name)
+        self.__cursor.execute("SELECT id_pessoa, nome, data_admissao FROM " + self.__table_name)
         return self.__cursor.fetchall()
 
+    def insert(self, data):
+        values = (data["nome"], data["rg"], data["cpf"], data["data_nascimento"], data["data_admissao"], data["funcao"])
+        query = "INSERT INTO " + self.__table_name \
+        + " (nome, rg, cpf, data_nascimento, data_admissao, funcao) " \
+        + "VALUES (%s, %s, %s, %s, %s, %s)"
+        self.__cursor.execute(query, values)
+        self.__db.commit()
+
 database = Database()
-print(database.search_all())
+# print(type(database.search_all()[0][1]))
+dados = {
+    "nome": "Joao",
+    "rg": "12345",
+    "cpf": "99999999",
+    "data_nascimento": '1996-03-10',
+    "data_admissao": '2021-2-27',
+    "funcao": 10
+}
+database.insert(dados)
+# print(database.search_all())
